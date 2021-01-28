c883a2c77c4464e449ac8a95a368ce096a35af82
BasharDlaleh <bb556119@gmail.com>
2021-01-28 12:17:46 +0000

    add region spec

--- app/Http/Controllers/API/Business2Controller.php
+++ app/Http/Controllers/API/Business2Controller.php
@@ -38,17 +38,22 @@ class Business2Controller extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Business2 $business2)
+    public function show(Business2 $business2, Request $request)
     {
+        if($request->region_id == 0)
         $clients = $business2->client()->select('client_id', 'client_date_entry', 'client_name', 'client_address', 'client_mobile1', 'client_image', 'client_star')
         ->orderBy('client_star', 'DESC')->orderBy('client_date_entry', 'DESC')
         ->orderBy('client_visible', 'DESC')->orderBy('client_date_entry', 'DESC')
         ->paginate(self::paginate);
         
-        // $clients = $clients->map(function ($client) {
-        //    $client->client_image = '/storage/client/'.$client->client_image;
-        //    return $client;
-        // });
+        else
+        $clients = $business2->client()->select('client_id', 'client_date_entry', 'client_name', 'client_address', 'client_mobile1', 'client_image', 'client_star')
+        ->where('client_region', $request->region_id)
+        ->orderBy('client_star', 'DESC')->orderBy('client_date_entry', 'DESC')
+        ->orderBy('client_visible', 'DESC')->orderBy('client_date_entry', 'DESC')
+        ->paginate(self::paginate);
+        
+        
         
         return response()->json($clients);
     }
